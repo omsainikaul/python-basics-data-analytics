7e83effffc7ea41060591799881c7323e70a9834
Om Saini <omsainikaul@gmail.com>
2026-08-16 16:52:00 +0530

    Delete 03_strings/STRINGS_AND_STRINGS_METHODS directory

--- 03_strings/STRINGS_AND_STRINGS_METHODS/f_string.py
+++
@@ -1,8 +0,0 @@
-name = input("enter your name : ")
-work = input("Where do you work? : ")
-city = input("Where do you live? : ")
-
-#Om works at TCS and lives in Delhi
-#print (name + " works at" + work + "and lives in" + city)
-
-print(f"{name} works at {work} and lives in {city}")
