9abbdcbf519b75f82ff980ac2625f4f7ed279019
Om Saini <omsainikaul@gmail.com>
2026-08-19 22:12:47 +0530

    Add Python functions

---
+++ 05_functions/FUNCTIONS/basics_3_functions.py
@@ -0,0 +1,7 @@
+def greet(fname, lname):
+    print("Good Morning!", fname, lname)
+    print("How are you?")
+    print("Thank You!")
+
+greet("Rohan", "Das")   #will place rohan in place of fname and das in place of lname
+greet("Om", "Saini")    #will place om in place of fname and saini in place of lname 
