a5b7db930ca9a5af0e2be396201f636551bee183
Om Saini <omsainikaul@gmail.com>
2026-08-21 12:21:49 +0530

    Add Python variable scope

---
+++ 06_variables_scope/LOCAL_AND_GLOBAL_VARIABLES/local_and_global.py
@@ -0,0 +1,9 @@
+def value():
+    x = 11      #local variable
+    print(x)
+
+value()        #prints value from local variable
+
+x = 78          #global variable
+
+value()        #global varible can't change the local varible value
